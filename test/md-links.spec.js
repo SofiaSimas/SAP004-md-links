dfd60312980a5295f15f010bb27c10cb210c22dc
Sofia <sofiasimas1992@gmail.com>
2020-07-12 13:35:31 -0300

    add md links teste

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,48 @@
-const mdLinks = require('../');
+const mdLinks = require('../md-links');
+const expectedOutput = require('./mocks/expectedOutput')
+const expectedValidateOutput = require('./mocks/expectedValidateOutput')
+const validateLink = require('../validate-link');
+const mdlinks = require('../md-links');
+
+jest.mock('../validate-link')
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  test('should read links from file', (done) => {
+    mdLinks('test/test-file.md')
+      .then((result)=> {
+        expect(result).toStrictEqual(expectedOutput)
+        done()
+      })
   });
+
+  test('should read links from file and validate them', (done) => {
+    validateLink.mockImplementation((link) => {
+      return new Promise((resolve) => {
+        resolve({
+          ...link,
+          statusCode: 200,
+          success: 'ok'
+        })
+      })
+    })
+    const options = {
+      validate:true,
+    }
+    mdLinks('test/test-file.md', options)
+      .then((result) => {
+    
+        expect(result).toStrictEqual(expectedValidateOutput)
+        done()
+      })
+  })
+
+  test('should throw error when file is not found', (done) => {
+    mdLinks('batatinha.md').catch((error) => {
+      expect(error.message).toBe('Ocorreu um erro ao ler o arquivo')
+      done()
+    })
+  })
 
 });
---
+++ test/mocks/expectedValidateOutput.js
@@ -0,0 +1,137 @@
+const expectedValidateOutput = [
+  {
+    href: 'https://nodejs.org/pt-br/about/',
+    text: "Sobre Node.js - DocumentaC'C#o oficial",
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    text: "Node.js file system - DocumentaC'C#o oficial",
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    text: "Node.js http.get - DocumentaC'C#o\noficial",
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://pt.wikipedia.org/wiki/Node.js',
+    text: 'Node.js - Wikipedia',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://medium.freecodecamp.org/what-exactly-is-node-js-ae36e97449f5',
+    text: 'What exactly is Node.js? -\nfreeCodeCamp',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://www.opus-software.com.br/node-js/',
+    text: 'Node.js b\u0013 O que C), como funciona e quais as\nvantagens',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://www.hostinger.com.br/tutoriais/o-que-e-npm',
+    text: 'O que C) npm',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175',
+    text: 'MC3dulos, librerC-as, paquetes, frameworks... B?cuC!l es la\ndiferencia?',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://medium.com/@alcidesqueiroz/javascript-ass%C3%ADncrono-callbacks-promises-e-async-functions-9191b8272298',
+    text: 'JavaScript assC-ncrono: callbacks, promises e async\nfunctions',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://docs.npmjs.com/getting-started/what-is-npm',
+    text: 'NPM',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://docs.npmjs.com/getting-started/publishing-npm-packages',
+    text: 'Publicar\npackage',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://docs.npmjs.com/getting-started/publishing-npm-packages',
+    text: 'Criando um mC3dulo\nNode.js',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback',
+    text: 'Ler um\narquivo',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback',
+    text: 'Ler um\ndiretC3rio',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    text: 'Path',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://medium.com/henriquekuwai/criando-sua-cli-com-node-js-d6dee7d03110',
+    text: 'Criando sua CLI com\nNode.js',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://regexcrossword.com/challenges/beginner/puzzles/1',
+    text: 'Regex Crossword',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://regex101.com/',
+    text: 'Online Regex Tester',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  },
+  {
+    href: 'https://regexr.com/',
+    text: 'Regexr',
+    file: 'test/test-file.md',
+    success: 'ok',
+    statusCode: 200
+  }
+]
+
+module.exports = expectedValidateOutput
